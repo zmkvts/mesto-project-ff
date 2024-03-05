a5d7a66881bafce6d0ea4f9c21fd4d976b768b1c
zmkvts <dzamkovets@yandex.ru>
2024-03-05 17:38:18 +0300

    функция добавления карточки и удаления карточки

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/zmkvts/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const plasesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
+function addCard(cardName, cardLink) {
+  // @todo: Темплейт карточки
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  // @todo: DOM узлы
+  cardItem.querySelector('.card__title').textContent = cardName;
+  cardItem.querySelector('.card__image').src = cardLink;
+  
+  const deleteButton = cardItem.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {
+    deleteCard(cardItem);
+  });
+  // @todo: Функция создания карточки
+  plasesList.append(cardItem); 
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardItem) {
+  cardItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(card) {
+  addCard(card.name, card.link);
+});
