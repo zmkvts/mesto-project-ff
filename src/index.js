e9cb783e61019a7d3fb00b49528d557ed2c2d2f2
zmkvts <dzamkovets@yandex.ru>
2024-04-01 15:03:41 +0300

    исправил удаление и переименовал функции

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const cardContainer = document.querySelector('.places__list');
 
 const popupEdit = document.querySelector('.popup_type_edit');
 const editProfileButton = document.querySelector('.profile__edit-button');
-const closePopupEdit = popupEdit.querySelector('.popup__close');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescr = document.querySelector('.profile__description');
 const formEditElement = document.forms['edit-profile'];
@@ -19,7 +18,6 @@ const jobInput = formEditElement['description'];
 
 const popupNewCard = document.querySelector('.popup_type_new-card');
 const addCardButton = document.querySelector('.profile__add-button');
-const closePopupNewCard = popupNewCard.querySelector('.popup__close');
 const formNewCard = document.forms['new-place'];
 const placeInput = formNewCard['place-name'];
 const linkInput = formNewCard['link'];
@@ -28,7 +26,8 @@ const linkInput = formNewCard['link'];
 const popupImage = document.querySelector('.popup_type_image');
 const popupImageData = popupImage.querySelector('.popup__image');
 const popupImageCaption = popupImage.querySelector('.popup__caption');
-const closePopupImage = popupImage.querySelector('.popup__close');
+
+const popups = document.querySelectorAll('.popup');
 
 
 // @todo: Вывести карточки на страницу
@@ -41,18 +40,6 @@ editProfileButton.addEventListener('click', function() {
   openPopup(popupEdit);
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileDescr.textContent;
-});
-
-// закрыть попап Edit кликом на крестик или Esc
-closePopupEdit.addEventListener('click', function() {
-  closePopup(popupEdit);
-});
-
-// закрыть попап Edit кликом на оверлей
-popupEdit.addEventListener ('click', function(evt) {
-  if (evt.target === evt.currentTarget) {
-    closePopup(popupEdit);
-  }
 });
 
 //обработчик события формы Edit
@@ -71,33 +58,20 @@ addCardButton.addEventListener('click', function() {
   openPopup(popupNewCard);
 })
 
-// закрыть попап Add кликом на крестик или Esc
-closePopupNewCard.addEventListener('click', function() {
-  closePopup(popupNewCard);
-});
-
-
-// закрыть попап newCard кликом на оверлей
-popupNewCard.addEventListener ('click', function(evt) {
-  if (evt.target === evt.currentTarget) {
-    closePopup(popupNewCard);
-  }
-});
-  
 //обработчик события формы newCard
-function handleFormNewCardSubmit(evt) {
+function addNewCard(evt) {
   evt.preventDefault();
   const newCard = {
     name: placeInput.value,
     link: linkInput.value
   };
-  cardContainer.prepend(createCard(newCard));
+  cardContainer.prepend(createCard(newCard, { deleteCard, likeCard, handleImageClick }));
   formNewCard.reset();
   closePopup(popupNewCard);
 }
   
 //слушатель на submit формы newCard
-formNewCard.addEventListener('submit', handleFormNewCardSubmit);
+formNewCard.addEventListener('submit', addNewCard);
 
 //увеличить картинку
 function handleImageClick(cardData) {
@@ -105,16 +79,68 @@ function handleImageClick(cardData) {
   openPopup(popupImage)
 }
 
+//присвоить картинке название и данные от карточки  
 function setImageData(cardData) {
   popupImageData.src = cardData.link;
   popupImageData.alt = cardData.name;
   popupImageCaption.textContent = cardData.name;
 }
 
-//закрыть попап Image
-closePopupImage.addEventListener('click', function() {
-  closePopup(popupImage);
-});
+// Закрытие попапов
+
+// Геннадий, большое спасибо за комментарий про закрытие попапов, головой я понимал, 
+// что общие классы как раз для этого и нужны, но перевести понимание в код не сумел))
+
+popups.forEach((popup) => {
+    popup.addEventListener('mousedown', (evt) => {
+        if (evt.target.classList.contains('popup_opened')) {
+            closePopup(popup)
+        }
+        if (evt.target.classList.contains('popup__close')) {
+          closePopup(popup)
+        }
+        if (evt.target === evt.currentTarget) {
+          closePopup(popup);
+        }
+    })
+})
+
+
+
+//const closePopupEdit = popupEdit.querySelector('.popup__close');
+// закрыть попап Edit кликом на крестик или Esc
+// closePopupEdit.addEventListener('click', function() {
+//   closePopup(popupEdit);
+// });
+
+// // закрыть попап Edit кликом на оверлей
+// popupEdit.addEventListener ('click', function(evt) {
+//   if (evt.target === evt.currentTarget) {
+//     closePopup(popupEdit);
+//   }
+// });
+
+
+//const closePopupNewCard = popupNewCard.querySelector('.popup__close');
+// // закрыть попап Add кликом на крестик или Esc
+// closePopupNewCard.addEventListener('click', function() {
+//   closePopup(popupNewCard);
+// });
+
+
+// // закрыть попап newCard кликом на оверлей
+// popupNewCard.addEventListener ('click', function(evt) {
+//   if (evt.target === evt.currentTarget) {
+//     closePopup(popupNewCard);
+//   }
+// });
+  
+
+//const closePopupImage = popupImage.querySelector('.popup__close');
+// //закрыть попап Image
+// closePopupImage.addEventListener('click', function() {
+//   closePopup(popupImage);
+// });
 
 
 
